09030e7c39ebe37e82051200f8c5a859f339997c
Heyashuu <90177888+Heyashuu@users.noreply.github.com>
2024-01-30 21:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (10.46%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,38 +1,22 @@
-class Solution
-{
-    public:
-        vector<int> twoSum(vector<int> &numbers, int target)
-        {
-            vector<int> ans;
-            int n = numbers.size() - 1;
-            for (int i = 0; i <= n; i++)
-            {
-                int left = i + 1;
-                int right = n;
-                int mid = (left + right) / 2;
-                while (left <= right)
-                {
-                    mid = (left + right) / 2;
-                    if (numbers[mid] + numbers[i] == target)
-                    {
-                        ans.push_back(i + 1);
-                        ans.push_back(mid + 1);
-                        return ans;
-                    }
-                    else if (numbers[mid] + numbers[i] > target)
-                    {
-                        right = mid - 1;
-                    }
-                    else
-                    {
-                        left = mid + 1;
-                    }
-                }
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans ; 
+        int n = numbers.size()-1 ; 
+        int j = 0 ; 
+        int k = n ; 
+        while(j<k){
+            int l = numbers[j]+numbers[k] ; 
+            if(numbers[j]+numbers[k]==target){
+                ans.push_back(j+1);
+                ans.push_back(k+1);
+                return ans ; 
             }
-            if (ans.empty())
-            {
-                ans.push_back(-1);
+            else if (numbers[j]+numbers[k]>target){
+                k--;
             }
-            return ans;
+            else{j++;}
         }
+        return ans ; 
+    }
 };
