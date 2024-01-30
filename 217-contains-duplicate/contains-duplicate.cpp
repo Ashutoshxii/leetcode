aa2dc6c52e2e47219d71d31ffa2be45be0940356
Heyashuu <90177888+Heyashuu@users.noreply.github.com>
2024-01-30 19:33:57 +0530

    Time: 117 ms (33.59%) | Memory: 75.2 MB (6.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -7,7 +7,7 @@ public:
             mpp[nums[i]]++;
         }
         for(int i = 0 ; i <nums.size() ; i++){
-            if(mpp[nums[i]]>=2){
+            if(mpp[nums[i]]>1){
                 return true ; 
             }
         }
