546b0de64abfbc5e9be178fa8dffdc32923a8583
Heyashuu <90177888+Heyashuu@users.noreply.github.com>
2024-01-31 19:12:56 +0530

    Time: 44 ms (88.87%) | Memory: 48.7 MB (7.28%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        var f = "Hello World";
+        return f ;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
