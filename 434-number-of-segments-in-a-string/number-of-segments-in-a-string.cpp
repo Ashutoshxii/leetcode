71c772abdbd104f16fd474fccc051c4d95f7a2cb
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2025-02-01 00:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (97.89%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSegments(string s) {
+         int count = 0 ; 
+         for(int i =0 ; i < s.size() ; i++){
+            if(s[i]!=' '){
+                while(s[i]!=' '&&i<s.size()){
+                    i++ ; 
+                }
+                count++ ; 
+            }
+         }
+         return count ; 
+    }
+};
