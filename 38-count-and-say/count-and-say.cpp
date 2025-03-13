93c3fd23d1e50990e82255b61d700c3d2ab94c73
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2025-03-13 22:52:10 +0530

    Time: 7 ms (21.37%) | Memory: 10.1 MB (47.97%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    string compressor(string k ){
+     
+        string ans = "" ; 
+        if(k.size()==1){
+            return "1"+k ; 
+        }
+        int i = 0 ; 
+         while(i < k.size()) {
+            int count = 1 ;
+            while(i+1 < k.size() && k[i]==k[i+1]){
+                count++ ; 
+                i++ ; 
+            }
+    
+            ans += to_string(count) + k[i];
+   
+            i++;
+        }
+        return ans ; 
+    }
+
+    string RLE(int n){
+        if(n==1){
+            return "1" ; 
+        }
+        
+        return compressor(RLE(n-1)) ; 
+         
+    }
+    string countAndSay(int n) {
+         return RLE(n) ; 
+    }
+};
