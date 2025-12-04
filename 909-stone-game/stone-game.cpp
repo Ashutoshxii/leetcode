e9cf36a7cb0fb9680be17ce15da9c37827ef5a34
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2025-12-04 13:32:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.45%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+       
+    bool stoneGame(vector<int>& piles) {
+        int alice = 0 ; 
+        int bob = 0 ; 
+
+        // solve(piles,0,piles.size()-1,alice,bob) ; 
+        return true ; 
+
+    }
+};
