706ff79e98a0d3d400d01c15efd4fd737ba538ea
Ashutosh Jaiswal <90177888+Ashutoshxii@users.noreply.github.com>
2026-03-14 20:32:36 +0530

    Time: 34 ms (5.07%) | Memory: 21.1 MB (5.25%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void solve(int ind,vector<int>&can,int t,vector<int>path,vector<vector<int>>&ans){
+        if(t==0)ans.push_back(path) ; 
+        if(t<0)return ; 
+        for(int i = ind ; i < can.size() ;i++){
+            if(i>ind&&can[i-1]==can[i])continue ; 
+            path.push_back(can[i]) ; 
+            solve(i+1,can,t-can[i],path,ans) ; 
+            path.pop_back() ; 
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans ;
+        vector<int>path ;
+        sort(candidates.begin(),candidates.end()) ;  
+        solve(0,candidates,target,path,ans) ;
+        return ans ; 
+    }
+};
